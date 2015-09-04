5f3dc2e586a33fb62795b827f1cfec8ff6f84fc2
Chanala Wilangka <yaicw49@hotmail.com>
2015-09-05 01:16:27 +0700

    Finish nav bar

--- views/header.php
+++ views/header.php
@@ -1,6 +1,24 @@
 <!-- Header ************************** -->
 <header class="entry-header">
-        <div class="inner">
-            <h1 class="entry-title">MIM ********************************</h1>
+    <div class="inner">
+        <div class="nav-sec1">
+            <img src="images/header/mim_logo.jpg" alt="" class="logo">
+            <p class="nav-title">MASTER’S DEGREE
+                <br>PROGRAM IN MARKTING</p>
         </div>
-    </header>
+        <div class="nav-sec2">
+            <ul class="nav-ul">
+                <li class="nav-btn1">
+                    <a href="#">
+                        <p class="nav1-btn1">JOIN OPEN HOUSE</p>
+                    </a>
+                </li>
+                <li class="nav-btn2">
+                    <a href="#">
+                        <p class="nav1-btn2">APPLY</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
