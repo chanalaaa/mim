8ef40527811990341986590cc2c8afdf41418cbd
Chanala Wilangka <yaicw49@hotmail.com>
2015-09-04 12:52:58 +0700

    Finish s3 + replace new img from p'max

--- views/content.php
+++ views/content.php
@@ -67,13 +67,13 @@
                         </div>
                         <div class="box2 s2_3" data-190p="opacity:0;" data-338p="transform:translate(0%,18%);" data-355p="transform:translate(0%,-12%);opacity:1;" data-375p="transform:translate(0%,-12%);opacity:1;" data-390p="transform:translate(0,-28%);opacity:0;">
                             <ul>
-                                <li><img class="s3-ico" src="images/content/s3/s31.png" alt=""></li>
+                                <li><img class="s3-ico" src="images/content/s3/s32.png" alt=""></li>
                                 <li>
                                     <p class="s3-title">YOUR MASTERY</p>
                                 </li>
                                 <li>
-                                    <p class="s3-info">Learn from Business Masters to
-                                        <br>Master the Business</p>
+                                    <p class="s3-info">Learn from business masters to
+                                        <br>master the business</p>
                                 </li>
                                 <li>
                                     <a href="#">
@@ -84,7 +84,7 @@
                         </div>
                         <div class="box2 s2_5" data-387p="transform:translate(0%,8%);opacity:0;" data-400p="transform:translate(0%,-20%);opacity:1;" data-400p="transform:translate(0%,-20%);opacity:1;" data-475p="transform:translate(0%,-60%);opacity:0;">
                             <ul>
-                                <li><img class="s3-ico" src="images/content/s3/s31.png" alt=""></li>
+                                <li><img class="s3-ico3" src="images/content/s3/s33.png" alt=""></li>
                                 <li>
                                     <p class="s3-title">YOUR EMPOWERMENTS</p>
                                 </li>
@@ -113,7 +113,10 @@
                                 <p>GO</p>
                             </div>
                         </div>
-                        <div class="box1 s2_2" data-520p="transform:translate(0,0%);opacity:1;" data-548p="transform:translate(0,-20%);opacity:0;">S4 1111111111111
+                        <div class="box1 s2_2" data-520p="transform:translate(0,0%);opacity:1;" data-548p="transform:translate(0,-20%);opacity:0;"><ul>
+                            <li><p class="s4-title">SUCCESS!</p></li>
+                            <li><p class="s4-info">Over 3,000 graduates working in key roles in Thailand and globally</p></li>
+                        </ul>
                         </div>
                         <div class="box1 s2_3" data-290p="opacity:0;" data-538p="transform:translate(0%,18%);" data-555p="transform:translate(0%,-12%);opacity:1;" data-575p="transform:translate(0%,-12%);opacity:1;" data-590p="transform:translate(0,-28%);opacity:0;">S4 222222222222222222</div>
                         <div class="box1 s2_5" data-587p="transform:translate(0%,8%);opacity:0;" data-600p="transform:translate(0%,-17%);opacity:1;" data-600p="transform:translate(0%,-17%);opacity:1;" data-675p="transform:translate(0%,-60%);opacity:0;">S4 3333333333333333333</div>
