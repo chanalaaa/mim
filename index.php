b8b48cbc947eb6ac53f90d56b51231fa35e8105a
Chanala Wilangka <yaicw49@hotmail.com>
2015-09-15 10:27:08 +0700

    edit title

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ ob_start('ob_gzhandler');
     <html>
 
     <head>
-        <title>MIM ADMISSTION</title>
+        <title>MIM ADMISSION</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width">
         <script type="text/javascript" src="js/min/jquery-1.11.3.min.js?v=1001"></script>
@@ -28,6 +28,22 @@ ob_start('ob_gzhandler');
         </div>
         </script>
         <script type="text/javascript" src="js/min/main.min.js"></script>
+        <script>
+        (function(i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function() {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-63096423-1', 'auto');
+        ga('send', 'pageview');
+        </script>
     </body>
 
     </html>
